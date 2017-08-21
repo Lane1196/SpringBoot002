fa5ee4b98e42d3ce934d3865cf2873f77118eaf6
Lane <wzhijiang@163.com>
2017-08-22 00:02:30 +0800

    sql修改

--- src/sql/dev.sql
+++ src/sql/dev.sql
@@ -13,22 +13,25 @@ File Encoding         : 65001
 Date: 2017-08-21 22:50:05
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `pwd` varchar(255) NOT NULL,
-  `age` int(11) DEFAULT NULL,
-  `gender` int(11) NOT NULL,
+  `id`     INT(11)      NOT NULL AUTO_INCREMENT,
+  `name`   VARCHAR(255) NOT NULL,
+  `pwd`    VARCHAR(255) NOT NULL,
+  `age`    INT(11)               DEFAULT NULL,
+  `gender` INT(11)      NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'Lane', '123', '12', '1');
+INSERT INTO `user` VALUES ('1', 'Lane001', '123', '18', '1');
